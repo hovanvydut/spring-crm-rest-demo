5c55b834174e4bddf69d0c092ee6de75c9e9a2e4
hovanvydut <hovanvydut@gmail.com>
2021-01-09 17:31:43 +0700

    add security role based acess for api

---
+++ src/main/java/dut/hovanvy/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,16 @@
+package dut.hovanvy.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	public SecurityWebApplicationInitializer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SecurityWebApplicationInitializer(Class<?>... configurationClasses) {
+		super(configurationClasses);
+		// TODO Auto-generated constructor stub
+	}
+
+}
